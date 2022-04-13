d910ae92f26a173a80ba93d3de3e0e1ba66b0617
Smit Patel <sbhikadiya892@rku.ac.in>
2022-04-13 16:51:48 +0530

    Completed pagination and searching for customer

--- customers.php
+++ customers.php
@@ -2,7 +2,20 @@
 session_start();
 require("./handler/CustomerHandler.php");
 $obj = new CustomerHandler();
-$customers = $obj->getCustomers();
+
+// for pagination
+$currntPage = 1;
+$showRecords = 5;
+$search = '';
+if (isset($_GET["page"])) {
+	$currntPage = $_GET["page"];
+	$showRecords = isset($_GET["show"]) ? $_GET["show"] : $showRecords;
+	$search = isset($_GET["search"]) ? $_GET["search"] : $search;
+}
+
+$totalRecords = $obj->TotalCustomers($search);
+$customers = $obj->getCustomers($search, (($currntPage - 1) * $showRecords), $showRecords);
+
 $msg = '';
 $error = false;
 if (isset($_SESSION["result"])) {
@@ -33,26 +46,6 @@ if (isset($_SESSION["result"])) {
 	include_once("./includes/header.php");
 	?>
 
-
-	<!-- Modal -->
-	<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header">
-					<h5 class="modal-title" id="exampleModalLabel">Customer</h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body">
-					
-				</div>
-
-			</div>
-		</div>
-	</div>
-
-
 	<div id="layoutSidenav">
 		<div id="layoutSidenav_nav">
 			<?php
@@ -63,7 +56,7 @@ if (isset($_SESSION["result"])) {
 			<main>
 				<div class="container-fluid">
 					<h2 class="mt-30 page-title">Customers</h2>
-					<ol class="breadcrumb mb-30">
+					<ol class="breadcrumb">
 						<li class="breadcrumb-item"><a href="index.php">Dashboard</a></li>
 						<li class="breadcrumb-item active">Customers</li>
 					</ol>
@@ -79,11 +72,19 @@ if (isset($_SESSION["result"])) {
 					}
 					?>
 
+					<nav class="navbar navbar-light bg-light justify-content-between">
+						<div></div>
+						<div class="form-inline">
+							<input class="form-control mr-sm-2" type="search" placeholder="Search By Name" aria-label="Search" value="<?= $search ?>">
+							<button class="status-btn hover-btn my-2 my-sm-0" id="searchRec" type="submit">Search</button>
+						</div>
+					</nav>
+
 					<div class="row">
 						<div class="col-lg-12 col-md-12">
 							<div class="card card-static-2 mb-30">
 								<div class="card-title-2">
-									<h4><b>All Customers<b></h4>
+									<h4><b>All Customers</b></h4>
 								</div>
 								<div class="card-body-table px-3">
 									<div class="table-responsive">
@@ -99,22 +100,22 @@ if (isset($_SESSION["result"])) {
 												</tr>
 											</thead>
 											<tbody>
-											<?php
+												<?php
 												if (count($customers) > 0) {
 													$srno = 1;
 													foreach ($customers as $cust) {
 												?>
-												<tr>
-													<td><?=$srno++?></td>
-													<td><?=$cust["username"]?></a></td>
-													<td><?=$cust["email"]?></td>
-													<td><?=$cust["modifiedDate"]?></td>
-													<td><?=$cust["createdDate"]?></td>
-													<td class="action-btns">
-														<a href="./view_customer.php?view=<?=$cust["id"]?>" class="view-shop-btn" title="View"><i class="fas fa-eye"></i></a>
-														<a href="./handler/requestHandler.php?dCustomer=<?=$cust["id"]?>" class="delete-btn deleteRow" title="Edit"><i class="fas fa-trash-alt"></i></a>
-													</td>
-												</tr>
+														<tr>
+															<td><?= $cust["id"] ?></td>
+															<td><?= $cust["username"] ?></a></td>
+															<td><?= $cust["email"] ?></td>
+															<td><?= $cust["modifiedDate"] ?></td>
+															<td><?= $cust["createdDate"] ?></td>
+															<td class="action-btns">
+																<a href="./view_customer.php?view=<?= $cust["id"] ?>" class="view-shop-btn" title="View"><i class="fas fa-eye"></i></a>
+																<a href="./handler/requestHandler.php?dCustomer=<?= $cust["id"] ?>" class="delete-btn deleteRow" title="Edit"><i class="fas fa-trash-alt"></i></a>
+															</td>
+														</tr>
 												<?php
 													}
 												} else {
@@ -123,6 +124,55 @@ if (isset($_SESSION["result"])) {
 												?>
 											</tbody>
 										</table>
+										<div class="div-pagination mt-3 d-flex justify-content-between">
+											<div class="page-select">
+												show&nbsp;
+												<select style="height: 35px; width:60px; border:1px solid #0056b3; color:#0056b3; border-radius:4px" name="" id="show-record">
+													<?php
+													foreach ([5, 10, 25, 50] as $rec) {
+														$selected = '';
+														if ($rec == $showRecords) {
+															$selected = "selected";
+														}
+														echo "<option value='$rec' $selected>$rec</option>";
+													}
+													?>
+
+												</select>&nbsp;&nbsp;entries, Total Records: <span id="totalrecords"><?= $totalRecords ?></span>
+											</div>
+											<div>
+												<nav aria-label="Page navigation example">
+													<ul class="pagination">
+														<li class="page-item">
+															<a class="page-link" aria-label="Previous" data-action="left">
+																<span aria-hidden="true">&laquo;</span>
+																<span class="sr-only">Previous</span>
+															</a>
+														</li>
+														<?php
+														for ($i = $currntPage; $i <= $currntPage + 2; $i++) {
+															$active = "";
+															$disabled = '';
+															if ($i == $currntPage) {
+																$active = "active";
+															}
+															if (ceil($totalRecords / $showRecords) < $i) {
+																$disabled = "disabled";
+															}
+															echo '<li class="page-item ' . $active . '"><a class="page-link ' . $disabled . '">' . $i . '</a></li>';
+														}
+														?>
+
+														<li class="page-item">
+															<a class="page-link" aria-label="Next" data-action="right">
+																<span aria-hidden="true">&raquo;</span>
+																<span class="sr-only">Next</span>
+															</a>
+														</li>
+													</ul>
+												</nav>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
--- handler/customerHandler.php
+++ handler/customerHandler.php
@@ -3,9 +3,21 @@ require_once("dbHandler.php");
 
 class CustomerHandler extends DBConnection
 {
-    function getCustomers()
+
+    function TotalCustomers($search=''){
+        $search_ = ($search=='') ? 1 : "username LIKE '%".$search."%'";
+        $sql = "SELECT COUNT(*) AS total FROM users WHERE $search_ AND status=0 ORDER BY id DESC";
+        $result = $this->getConnection()->query($sql);
+        if($result && $result->num_rows > 0){
+            return $result->fetch_assoc()["total"];
+        } 
+        return 0;
+    }
+
+    function getCustomers($search, $page, $show)
     {
-        $sql = "SELECT * FROM users WHERE status=0 ORDER BY id DESC";
+        $search_ = ($search=='') ? 1 : "username LIKE '%".$search."%'";
+        $sql = "SELECT * FROM users WHERE $search_ AND status=0 ORDER BY id DESC LIMIT $page, $show";
         $result = $this->getConnection()->query($sql);
         $records = [];
         if ($result->num_rows > 0) {
