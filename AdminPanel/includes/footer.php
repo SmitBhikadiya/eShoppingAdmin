5a5f724026821b454edb267a3447a058d604a221
Smit Patel <sbhikadiya892@rku.ac.in>
2022-04-15 18:10:21 +0530

    structure changed

--- AdminPanel/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="py-4 bg-footer mt-auto">
-    <div class="container-fluid">
-        <div class="d-flex align-items-center justify-content-between small">
-            <div class="text-muted-1">© 2022 <b>eShopping</b></div>
-        </div>
-    </div>
-</footer>
--- AdminPanel/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["adminlogin"]);
-    header("Location: signin.php");
-?>
