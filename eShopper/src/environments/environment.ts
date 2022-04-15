d0a6a1d0980373da530c92d19b9e0acdb47980f5
Smit Patel <sbhikadiya892@rku.ac.in>
2022-04-15 18:28:46 +0530

    changed url for api

--- eShopper/src/environments/environment.ts
+++ eShopper/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  API_SERVER_URL : "http://127.0.0.1/eShoppingAdmin/AdminPanel/api",
-  IMAGES_SERVER_URL : "http://127.0.0.1/eShoppingAdmin/AdminPanel/images",
+  API_SERVER_URL : "http://127.0.0.1/eShoppingAdmin/Admin/api",
+  IMAGES_SERVER_URL : "http://127.0.0.1/eShoppingAdmin/Admin/images",
   DEFAULT_LOAD_PRODUCT : 10
 };
